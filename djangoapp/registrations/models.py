136cfc05b4d59d52ac1ed32cece9bb3da77262d4
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-12-03 06:51:42 -0300

    setup models for registrations project

--- djangoapp/registrations/models.py
+++ djangoapp/registrations/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from python_dev_academy import settings
+
+
+class Student(AbstractUser):
+    registration_number = models.CharField(max_length=20, unique=True)
+
+    class Meta:
+        ordering = ["first_name", "last_name", "registration_number"]
+
+    def __str__(self):
+        return self.registration_number + " " + self.first_name + " " + self.last_name
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    students = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="courses")
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
