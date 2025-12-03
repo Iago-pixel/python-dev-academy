333e48c8fdd4395940f26c58e6a201080175749d
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-12-03 12:18:04 -0300

    Customizing admin panel

--- djangoapp/registrations/admin.py
+++ djangoapp/registrations/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from registrations.models import Course
+from registrations.models import Course, Student
+
+@admin.register(Student)
+class StudentAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("first_name", "last_name")}),)
+
 
 admin.site.register(Course)
