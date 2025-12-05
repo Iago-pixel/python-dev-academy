44a92567ceb72fe19f1251265c82845f363b46ae
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-12-05 09:03:28 -0300

    add index template

--- djangoapp/registrations/views.py
+++ djangoapp/registrations/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Course, Student
 
 def index(request):
-    return HttpResponse("<h1>Welcome to the Registrations App</h1>")
+    num_courses = Course.objects.count()
+    num_students = Student.objects.count()
+    context = {
+        "num_courses": num_courses,
+        "num_students": num_students,
+    }
+    return render(request=request, template_name="registrations/index.html", context=context)
